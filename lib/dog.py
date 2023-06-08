290c8f3dac5da7ad468e277e3477d26d60cebd25
Joyce Njoroge <joycewambuku33@gmail.com>
2023-06-08 21:38:49 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,55 @@
-from models import Dog
+import os
+from models import Dog, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-def create_table(base):
-    pass
+# Specify the path to the SQLite database file
+db_file = 'dogs.db'
+# Create an engine to connect to the database
+engine = create_engine(f"sqlite:///{db_file}", echo=True, future=True)
+
+# Create the tables if they don't exist
+Base.metadata.create_all(engine)
+
+# Create a session factory
+Session = sessionmaker(bind=engine)
+
+def create_table(Base, engine):
+    Base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    session.close()
+    session.bind.dispose()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).get(id)
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name==name, Dog.breed==breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
+
+if __name__ == '__main__':
+    # Create a session
+    session = Session()
+
+    # Perform database operations
+    dog = Dog(name='Fido', breed='Labrador')
+    save(session, dog)
+
+    # Close the session and the connection
+    session.close()
+    engine.dispose()
+
+    # Remove the database file
+    os.remove(db_file)
